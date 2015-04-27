d67b6e7d07477e07c71e2eeec655174a13ea0ba0
thurask <thuraski@hotmail.com>
2015-04-27 00:19:49 -0400

    update version

--- bbarchivist/bbconstants.py
+++ bbarchivist/bbconstants.py
@@ -1,7 +1,7 @@
 from os.path import abspath, dirname, join
 
 #: App version.
-_version = "1.2.2"
+_version = "1.2.3"
 #: Version of cap.exe bundled with app.
 _capversion = "3.11.0.22"
 #: Where cap.exe is. Should be in site-packages.
