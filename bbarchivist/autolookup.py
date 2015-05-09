cb0dfd2d8932ccd925bbff0b973941655bd1ed5a
thurask <thuraski@hotmail.com>
2015-05-09 12:48:35 -0400

    ctrl-c to break lookup loop, handle it nicely

--- bbarchivist/autolookup.py
+++ bbarchivist/autolookup.py
@@ -18,71 +18,76 @@ def do_magic(osversion, recurse=False):
     """
     print("~~~AUTOLOOKUP VERSION", bbconstants.VERSION + "~~~")
     print("")
-    while True:
-        swrelease = ""
-        a1rel = networkutils.software_release_lookup(osversion,
-                                                     bbconstants.SERVERS["a1"])
-        if a1rel != "SR not in system" and a1rel is not None:
-            a1av = "A1"
-        else:
-            a1av = "  "
-#         a2rel = networkutils.software_release_lookup(osversion,
-#                                                      bbconstants.SERVERS["a2"])
-        a2rel = "SR not in system"
-        if a2rel != "SR not in system" and a2rel is not None:
-            a2av = "A2"
-        else:
-            a2av = "  "
-        b1rel = networkutils.software_release_lookup(osversion,
-                                                     bbconstants.SERVERS["b1"])
-        if b1rel != "SR not in system" and b1rel is not None:
-            b1av = "B1"
-        else:
-            b1av = "  "
-        b2rel = networkutils.software_release_lookup(osversion,
-                                                     bbconstants.SERVERS["b2"])
-        if b2rel != "SR not in system" and b2rel is not None:
-            b2av = "B2"
-        else:
-            b2av = "  "
-        prel = networkutils.software_release_lookup(osversion,
-                                                    bbconstants.SERVERS["p"])
-        if prel != "SR not in system" and prel is not None:
-            pav = "PD"
-            # Hash software version
-            swhash = hashlib.sha1(prel.encode('utf-8'))
-            hashedsoftwareversion = swhash.hexdigest()
-            # Root of all urls
-            baseurl = "http://cdn.fs.sl.blackberry.com/fs/qnx/production/"
-            baseurl += hashedsoftwareversion
-            # Check availability of software release
-            avail = networkutils.availability(baseurl)
-            if avail:
-                available = "Available"
+    try:
+        while True:
+            swrelease = ""
+            a1rel = networkutils.software_release_lookup(osversion,
+                                                         bbconstants.SERVERS["a1"])  # @IgnorePep8
+            if a1rel != "SR not in system" and a1rel is not None:
+                a1av = "A1"
             else:
+                a1av = "  "
+    #         a2rel = networkutils.software_release_lookup(osversion,
+    #                                                      bbconstants.SERVERS["a2"])
+            a2rel = "SR not in system"
+            if a2rel != "SR not in system" and a2rel is not None:
+                a2av = "A2"
+            else:
+                a2av = "  "
+            b1rel = networkutils.software_release_lookup(osversion,
+                                                         bbconstants.SERVERS["b1"])  # @IgnorePep8
+            if b1rel != "SR not in system" and b1rel is not None:
+                b1av = "B1"
+            else:
+                b1av = "  "
+            b2rel = networkutils.software_release_lookup(osversion,
+                                                         bbconstants.SERVERS["b2"])  # @IgnorePep8
+            if b2rel != "SR not in system" and b2rel is not None:
+                b2av = "B2"
+            else:
+                b2av = "  "
+            prel = networkutils.software_release_lookup(osversion,
+                                                        bbconstants.SERVERS["p"])  # @IgnorePep8
+            if prel != "SR not in system" and prel is not None:
+                pav = "PD"
+                # Hash software version
+                swhash = hashlib.sha1(prel.encode('utf-8'))
+                hashedsoftwareversion = swhash.hexdigest()
+                # Root of all urls
+                baseurl = "http://cdn.fs.sl.blackberry.com/fs/qnx/production/"
+                baseurl += hashedsoftwareversion
+                # Check availability of software release
+                avail = networkutils.availability(baseurl)
+                if avail:
+                    available = "Available"
+                else:
+                    available = "Unavailable"
+            else:
+                pav = "  "
                 available = "Unavailable"
-        else:
-            pav = "  "
-            available = "Unavailable"
-        swrelset = set([a1rel, a2rel, b1rel, b2rel, prel])
-        for i in swrelset:
-            if i != "SR not in system" and i is not None:
-                swrelease = i
-                break
-        else:
-            swrelease = ""
-        if swrelease != "":
-            print("OS {} - SR {} - [{}|{}|{}|{}|{}] - {}".format(osversion,
-                                                                 swrelease,
-                                                                 pav,
-                                                                 a1av,
-                                                                 a2av,
-                                                                 b1av,
-                                                                 b2av,
-                                                                 available))
-        if not recurse:
-            break
-        else:
-            osversion = utilities.version_incrementer(osversion, 3)
-            swrelease = ""
-            continue
+            swrelset = set([a1rel, a2rel, b1rel, b2rel, prel])
+            for i in swrelset:
+                if i != "SR not in system" and i is not None:
+                    swrelease = i
+                    break
+            else:
+                swrelease = ""
+            if swrelease != "":
+                print("OS {} - SR {} - [{}|{}|{}|{}|{}] - {}".format(osversion,
+                                                                     swrelease,
+                                                                     pav,
+                                                                     a1av,
+                                                                     a2av,
+                                                                     b1av,
+                                                                     b2av,
+                                                                     available)
+                      )
+            if not recurse:
+                raise KeyboardInterrupt  # hack, but whateva, I do what I want
+            else:
+                osversion = utilities.version_incrementer(osversion, 3)
+                swrelease = ""
+                continue
+    except KeyboardInterrupt:
+        print("\nFINISHED!!!")
+        raise SystemExit
