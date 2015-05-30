024d58fd2dc597ec0b1a55861305f7b73d13c1ba
thurask <thuraski@hotmail.com>
2015-05-30 02:41:09 -0400

    edit init

--- bbarchivist/__init__.py
+++ bbarchivist/__init__.py
@@ -1,4 +1,7 @@
 from . import bbconstants  # constants/versions
 
+__title__ = "bbarchivist"
 __version__ = bbconstants.VERSION
 __author__ = "Thurask"
+__license__ = "Do whatever"
+__copyright__ = "2015 Thurask"
