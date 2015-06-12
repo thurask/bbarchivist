6f4e786f603f997d1cae85df6afe7b32ed916b41
thurask <thuraski@hotmail.com>
2015-06-12 01:47:50 -0400

    update version

--- bbarchivist/bbconstants.py
+++ bbarchivist/bbconstants.py
@@ -1,7 +1,7 @@
 import os.path  # for producing cap location constant
 
 #: App version.
-VERSION = "1.7.0"
+VERSION = "1.7.1"
 #: Version of cap.exe bundled with app.
 CAPVERSION = "3.11.0.22"
 #: Where cap.exe is. Should be in site-packages.
