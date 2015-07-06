2257c2b9df0ede5b5c30a3e869150558cb288923
thurask <thuraski@hotmail.com>
2015-07-06 03:39:02 -0400

    refactor lazyloader script

--- bbarchivist/lazyloader.py
+++ bbarchivist/lazyloader.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse  # parse arguments
+import sys  # load arguments
 import os  # path work
 import glob  # filename matching
 import shutil  # folder removal
@@ -9,6 +11,164 @@ from bbarchivist import barutils  # file operations
 from bbarchivist import bbconstants  # constants/versions
 from bbarchivist import networkutils  # download/lookup
 from bbarchivist import loadergen  # cap wrapper
+
+
+def main():
+    """
+    Parse arguments from argparse/questionnaire.
+
+    Invoke :func:`bbarchivist.lazyloader.do_magic` with those arguments.
+    """
+    if len(sys.argv) > 1:
+        parser = argparse.ArgumentParser(
+            prog="bb-lazyloader",
+            description="Create one autoloader for personal use.",
+            epilog="http://github.com/thurask/bbarchivist")
+        parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version="%(prog)s " +
+            bbconstants.VERSION)
+        parser.add_argument(
+                            "os",
+                            help="OS version, 10.x.y.zzzz")
+        parser.add_argument(
+                            "radio",
+                            help="Radio version, 10.x.y.zzzz",
+                            nargs="?",
+                            default=None)
+        parser.add_argument(
+                            "swrelease",
+                            help="Software version, 10.x.y.zzzz",
+                            nargs="?",
+                            default=None)
+        devgroup = parser.add_argument_group(
+            "devices",
+            "Device to load (one required)")
+        compgroup = devgroup.add_mutually_exclusive_group(required=True)
+        compgroup.add_argument(
+            "--stl100-1",
+            dest="device",
+            help="STL100-1",
+            action="store_const",
+            const=0)
+        compgroup.add_argument(
+            "--stl100-x",
+            dest="device",
+            help="STL100-2/3, P'9982",
+            action="store_const",
+            const=1)
+        compgroup.add_argument(
+            "--stl100-4",
+            dest="device",
+            help="STL100-4",
+            action="store_const",
+            const=2)
+        compgroup.add_argument(
+            "--q10",
+            dest="device",
+            help="Q10, Q5, P'9983",
+            action="store_const",
+            const=3)
+        compgroup.add_argument(
+            "--z30",
+            dest="device",
+            help="Z30, Classic, Leap",
+            action="store_const",
+            const=4)
+        compgroup.add_argument(
+            "--z3",
+            dest="device",
+            help="Z3",
+            action="store_const",
+            const=5)
+        compgroup.add_argument(
+            "--passport",
+            dest="device",
+            help="Passport",
+            action="store_const",
+            const=6)
+        parser.add_argument(
+            "--run-loader",
+            dest="autoloader",
+            help="Run autoloader after creation",
+            action="store_true",
+            default=False)
+        parser.add_argument(
+            "-f",
+            "--folder",
+            dest="folder",
+            help="Working folder",
+            default=None,
+            metavar="DIR")
+        parser.add_argument(
+            "-n",
+            "--no-download",
+            dest="download",
+            help="Don't download files",
+            action="store_false",
+            default=True)
+        args = parser.parse_args(sys.argv[1:])
+        if args.folder is None:
+            args.folder = os.getcwd()
+        if not utilities.is_windows():
+            args.autoloader = False
+        do_magic(
+            args.device,
+            args.os,
+            args.radio,
+            args.swrelease,
+            args.folder,
+            args.autoloader,
+            args.download)
+    else:
+        localdir = os.getcwd()
+        osversion = input("OS VERSION: ")
+        radioversion = input("RADIO VERSION (PRESS ENTER TO GUESS): ")
+        if not radioversion:
+            radioversion = None
+        softwareversion = input("SOFTWARE RELEASE (PRESS ENTER TO GUESS): ")
+        if not softwareversion:
+            softwareversion = None
+        while True:
+            inputlist = ["0=STL100-1",
+                         "1=STL100-2/3/P9982",
+                         "2=STL100-4",
+                         "3=Q10/Q5/P9983",
+                         "\n4=Z30/CLASSIC/LEAP",
+                         "5=Z3",
+                         "6=PASSPORT"]
+            try:
+                device = int(input(
+                                   "SELECTED DEVICE (" +
+                                   "; ".join(inputlist) +
+                                   "): ")
+                             )
+            except ValueError:
+                continue
+            else:
+                break
+        if utilities.is_windows():
+            autoloader = utilities.str2bool(
+                input("RUN AUTOLOADER (WILL WIPE YOUR DEVICE!)(Y/N)?: "))
+        else:
+            autoloader = False
+        print(" ")
+        do_magic(
+            device,
+            osversion,
+            radioversion,
+            softwareversion,
+            localdir,
+            autoloader,
+            True)
+        smeg = input("Press Enter to exit")
+        if smeg or not smeg:
+            raise SystemExit
+
+if __name__ == "__main__":
+    main()
 
 
 def do_magic(device, osversion, radioversion=None,
