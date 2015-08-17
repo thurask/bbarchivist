572c96951b480f589d1bd961a6c3ea0db1060ebb
thurask <thuraski@hotmail.com>
2015-08-17 00:03:43 -0400

    linkgen: option to use different radio with different sw release

--- bbarchivist/scripts/linkgen.py
+++ bbarchivist/scripts/linkgen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 
 import argparse  # parse arguments
 import sys  # load arguments
@@ -38,12 +38,20 @@ def grab_args():
                             help="Software version, 10.x.y.zzzz",
                             nargs="?",
                             default=None)
+        parser.add_argument(
+                            "-r",
+                            "--radiosw",
+                            dest="altsw",
+                            help="Radio software version, if not same as OS",
+                            nargs="?",
+                            default=None)
         parser.set_defaults()
         args = parser.parse_args(sys.argv[1:])
         linkgen_main(
             args.os,
             args.radio,
-            args.swrelease)
+            args.swrelease,
+            args.altsw)
     else:
         osversion = input("OS VERSION: ")
         radioversion = input("RADIO VERSION: ")
@@ -53,16 +61,23 @@ def grab_args():
             radioversion = None
         if not softwareversion:
             softwareversion = None
+        usealt = utilities.str2bool(input("USE ALTERNATE RADIO? Y/N: "))
+        if usealt:
+            altsw = input("RADIO SOFTWARE RELEASE: ")
+        else:
+            altsw = None
         linkgen_main(
             osversion,
             radioversion,
-            softwareversion)
+            softwareversion,
+            altsw)
         smeg = input("Press Enter to exit")
         if smeg or not smeg:
             raise SystemExit
 
 
-def linkgen_main(osversion, radioversion=None, softwareversion=None):
+def linkgen_main(osversion, radioversion=None,
+                 softwareversion=None, altsw=None):
     """
     Generate debrick/core/radio links for given OS, radio, software release.
 
@@ -74,6 +89,9 @@ def linkgen_main(osversion, radioversion=None, softwareversion=None):
 
     :param softwareversion: Software version, 10.x.y.zzzz. Can be guessed.
     :type softwareversion: str
+
+    :param altsw: Radio software release, if not the same as OS.
+    :type altsw: str
     """
     if radioversion is None:
         radioversion = utilities.version_incrementer(osversion, 1)
@@ -92,6 +110,11 @@ def linkgen_main(osversion, radioversion=None, softwareversion=None):
 
     # List of debrick urls
     osurls, coreurls, radiourls = textgenerator.url_generator(osversion, radioversion, softwareversion) #@IgnorePep8
+
+    if altsw:
+        x, y, radiourls = textgenerator.url_generator(osversion, radioversion, altsw)
+        del x
+        del y
 
     avlty = networkutils.availability(baseurl)
     textgenerator.write_links(softwareversion, osversion, radioversion,
