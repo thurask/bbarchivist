4fac1b740dac6a548ed452c5a0ba3f00cc35c2a6
thurask <thuraski@hotmail.com>
2015-09-15 00:34:31 -0400

    refactor tests

---
+++ tests/pseudocap/__init__.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env python3
+
+"""The unit test portion of bbarchivist."""
+
+from bbarchivist import bbconstants  # constants/versions
+
+__title__ = "bbarchivist.tests.pseudocap"
+__version__ = bbconstants.VERSION
+__author__ = "Thurask"
+__license__ = "Do whatever"
+__copyright__ = "2015 Thurask"
---
+++ tests/scriptutils/test_scriptutils.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+#pylint: disable = I0011, R0201, W0613, C0301
+"""Test the scriptutils module."""
