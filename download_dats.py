286a742997d59275ea40913a3d6e3fcb6d906240
thurask <thuraski@hotmail.com>
2016-05-04 12:31:36 -0400

    fix download_dats without needing dependencies

--- download_dats.py
+++ download_dats.py
@@ -3,7 +3,7 @@
 """Manually download dat files, if Git-LFS isn't working or something."""
 
 import os
-from bbarchivist.networkutils import download_bootstrap
+import requests
 from bbarchivist.bbconstants import CAP, CFP
 
 __author__ = "Thurask"
@@ -11,7 +11,31 @@ __license__ = "WTFPL v2"
 __copyright__ = "Copyright 2015-2016 Thurask"
 
 
-def download():
+def download(url, output_directory=None):
+    """
+    Download file from given URL.
+
+    :param url: URL to download from.
+    :type url: str
+
+    :param output_directory: Download folder. Default is local.
+    :type output_directory: str
+    """
+    if output_directory is None:
+        output_directory = os.getcwd()
+    lfname = url.split('/')[-1]
+    fname = os.path.join(output_directory, lfname)
+    with open(fname, "wb") as file:
+        req = requests.get(url, stream=True)
+        if req.status_code == 200:  # 200 OK
+            print("DOWNLOADING {0} [{1}]".format(lfname, fsize))
+            for chunk in req.iter_content(chunk_size=1024):
+                file.write(chunk)
+        else:
+            print("ERROR: HTTP {0} IN {1}".format(req.status_code, lfname))
+
+
+def main():
     """
     Download cap and cfp files, in lieu of Git-LFS.
     """
@@ -31,10 +55,11 @@ def download():
         files.append(basename + cfpfile)
     outdir = os.path.join(os.getcwd(), "bbarchivist")
     if files:
-        download_bootstrap(files, outdir)
+        for file in files:
+            download(file, outdir)
     else:
         print("NOTHING TO DOWNLOAD!")
 
 
 if __name__ == "__main__":
-    download()
+    main()
