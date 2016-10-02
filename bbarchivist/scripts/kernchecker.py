4fb5d799af1512a8858c8f91f1a48219495cc77c
thurask <thuraski@hotmail.com>
2016-10-02 18:56:29 -0400

    make kernchecker look nicer

--- bbarchivist/scripts/kernchecker.py
+++ bbarchivist/scripts/kernchecker.py
@@ -29,7 +29,14 @@ def kernchecker_main():
     tocheck = "UTILS" if args.utils else "KERNELS"
     print("\nCHECKING {0}...\n".format(tocheck))
     kernlist = networkutils.kernel_scraper(args.utils)
-    utilities.lprint(kernlist)
+    splitkerns = [x.split("/") for x in kernlist]
+    platforms = list({x[0] for x in splitkerns})
+    kerndict = {x: [] for x in platforms}
+    for kernel in splitkerns:
+        kerndict[kernel[0]].append("\t{0}".format(kernel[1]))
+    for board in kerndict.keys():
+        print(board)
+        utilities.lprint(sorted(kerndict[board], reverse=True))
     decorators.enter_to_exit(True)
 
 
