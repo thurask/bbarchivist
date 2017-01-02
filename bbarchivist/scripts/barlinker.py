396e01f477e8cc9af6e729b655ae89582b0d40a8
thurask <thuraski@hotmail.com>
2017-01-02 13:23:57 -0500

    add questionnaire to barlinker

--- bbarchivist/scripts/barlinker.py
+++ bbarchivist/scripts/barlinker.py
@@ -11,17 +11,30 @@ __license__ = "WTFPL v2"
 __copyright__ = "2016-2017 Thurask"
 
 
+def questionnaire():
+    """
+    Ask input from user.
+    """
+    software = input("SOFTWARE RELEASE (ex. 10.3.2.2848): ")
+    appname = input("APPLICATION NAME (ex. sys.android): ")
+    appver = input("APPLICATION VERSION (ex. 10.3.2.348): ")
+    print(utilities.create_bar_url(software, appname, appver, True))
+    
+
 def barlinker_main():
     """
     Wrap around :mod:`bbarchivist.utilities` link generation.
     """
-    parser = scriptutils.default_parser("bb-barlinker", "Bar file link generator.")
-    parser.add_argument("appname", help="Full app name (ex. 'sys.pim.calendar')")
-    parser.add_argument("appver", help="App version (ex. '10.3.2.371')")
-    parser.add_argument("software", help="Software version of OS")
-    parser.set_defaults()
-    args = parser.parse_args(sys.argv[1:])
-    print(utilities.create_bar_url(args.software, args.appname, args.appver, True))
+    if len(sys.argv) > 1:
+        parser = scriptutils.default_parser("bb-barlinker", "Bar file link generator.")
+        parser.add_argument("appname", help="Full app name (ex. 'sys.pim.calendar')")
+        parser.add_argument("appver", help="App version (ex. '10.3.2.371')")
+        parser.add_argument("software", help="Software version of OS")
+        parser.set_defaults()
+        args = parser.parse_args(sys.argv[1:])
+        print(utilities.create_bar_url(args.software, args.appname, args.appver, True))
+    else:
+        questionnaire()
     decorators.enter_to_exit(True)
 
 
