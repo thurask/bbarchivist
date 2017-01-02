fb6aebc5338e5f53e800a7cad17cb3915fb8875d
thurask <thuraski@hotmail.com>
2017-01-02 14:57:11 -0500

    add python 3.6 to pypi classifiers

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ def main():
               "Programming Language :: Python :: 3.3",
               "Programming Language :: Python :: 3.4",
               "Programming Language :: Python :: 3.5",
+              "Programming Language :: Python :: 3.6",
               "Programming Language :: Python :: 3",
               "Programming Language :: Python :: 3 :: Only",
               "Topic :: Utilities"
