06bb212956847b0f70f8409c9d67d9ebc4cba1f3
thurask <thuraski@hotmail.com>
2018-05-01 09:44:34 -0400

    7-zip 1805

--- pyinstaller_exe.py
+++ pyinstaller_exe.py
@@ -102,7 +102,7 @@ def get_sevenzip():
     """
     Get 7-Zip.
     """
-    szver = "1801"
+    szver = "1805"
     szurl = "http://www.7-zip.org/a/7z{0}-extra.7z".format(szver)
     psz = prep_seven_zip()
     if psz:
